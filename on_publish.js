4313d9577a4c2cfdbd77835801d47f6007fc641b
root <root@DESKTOP-UD2KKHP.localdomain>
2021-04-06 00:32:18 -0300

    webhook write to mysql

--- on_publish.js
+++ on_publish.js
@@ -1,12 +1,25 @@
 const express = require("express");
 const router = express.Router();
-const distinct = '';
+const mysql = require('./mysql');
 
-module.exports = router.post('/', (req, res, next)=>{
+module.exports = router.post('/', async (req, res, next)=>{
 	console.log('on_publish', req.body);
-	if(distinct !== req.body.payload){
-		console.log(Buffer.from(req.body.payload, 'base64').toString('ascii'))
-		distinct = req.body.payload
-	}
+	let json =  req.body;
+	let data = Buffer.from(json.payload, 'base64').toString('ascii').split('|');
+	
+	let assoclist;
+	if(data.length > 2)
+		assoclist = data[3];
+	else
+		assoclist = null;
+	
+	await mysql.execute('CALL SP_TELEMETRY(?, ?, ?, ?, ?, ?);', [
+		json.client_id,
+		json.username,
+		data[0],
+		data[1],
+		data[2],
+		assoclist
+	]).catch(console.log);
 	return res.status(200).send({"result": "ok"});
 });
