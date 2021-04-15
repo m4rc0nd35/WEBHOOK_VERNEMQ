d88e7ce1ba064b675267b3d612bc7a181df9034b
Jean Marcondes <hardware@mtsis.com.br>
2021-04-15 18:31:25 -0300

    Delete start.sh

--- start.sh
+++
@@ -1,6 +0,0 @@
-export MYSQL_USER=m4rc0nd35 && \
-export MYSQL_PASSWORD=207979dust && \
-export MYSQL_DB=vernemq_db && \
-export MYSQL_HOST=10.136.101.186 && \
-export MYSQL_PORT=3306 && \
-node main.js
